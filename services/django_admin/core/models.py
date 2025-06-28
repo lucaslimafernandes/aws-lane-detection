3148ee3aa826fdb067e99a482a5e6288c221c64f
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2025-06-28 18:41:19 -0300

    CHORE: core:models

--- services/django_admin/core/models.py
+++ services/django_admin/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class VideoUpload(models.Model):
+
+    upload_user  = models.ForeignKey(User, on_delete=models.CASCADE)
+    file         = models.FileField(upload_to="videos/")
+    file_name    = models.CharField(max_length=255)
+    fiel_format  = models.CharField(max_length=10)
+    duration     = models.FloatField(help_text="Duration in seconds")
+    frame_rate   = models.FloatField()
+    total_frames = models.IntegerField()
+    created_at   = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file_name
