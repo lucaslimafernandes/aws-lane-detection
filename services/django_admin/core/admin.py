7ada576326139cf09d74a0180a00ecb4e6f18149
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2025-06-28 18:44:55 -0300

    CHORE: core:admin

--- services/django_admin/core/admin.py
+++ services/django_admin/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import VideoUpload
 
-# Register your models here.
+
+@admin.register(VideoUpload)
+class VideoUploadAdmin(admin.ModelAdmin):
+
+    list_display    = ("file_name", "upload_user", "duration", "frame_rate", "total_frames", "created_at")
+    readonly_fields = ("duration", "frame_rate", "total_frames")
+
--- services/django_admin/core/models.py
+++ services/django_admin/core/models.py
@@ -7,7 +7,7 @@ class VideoUpload(models.Model):
     upload_user  = models.ForeignKey(User, on_delete=models.CASCADE)
     file         = models.FileField(upload_to="videos/")
     file_name    = models.CharField(max_length=255)
-    fiel_format  = models.CharField(max_length=10)
+    file_format  = models.CharField(max_length=10)
     duration     = models.FloatField(help_text="Duration in seconds")
     frame_rate   = models.FloatField()
     total_frames = models.IntegerField()
